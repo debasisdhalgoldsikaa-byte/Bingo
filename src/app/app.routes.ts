b650d67074df14767a9f188127d2f4b17ddfb1e5
debasisdhalgoldsikaa-byte <debasisdhalgoldsikaa@gmail.com>
2026-08-18 12:27:44 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { LogInComponent } from './components/log-in/log-in.component';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: '', component: LogInComponent},
     {path:'home', component: HomeComponent}
 ];
